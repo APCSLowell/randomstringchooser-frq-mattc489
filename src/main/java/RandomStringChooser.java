ea1d65ca7098d3db81c4df1adbfa67afacf23f6b
mattc489 <machan12@s.sfusd.edu>
2025-04-24 11:24:41 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,16 +1,20 @@
 import java.util.*;
-public class RandomStringChooser
-{
-  private ArrayList<String>words;
-  public RandomStringChooser(String[]wordArray){
-    words=new ArrayList<String>();
-    for(String w: wordArray)
-      words.add(w);
-  }
-  public String getNext(){
-    if(words.size()==0)
-      return "NONE";
-    int i=(int)(Math.random()*words.size());\
-    return words.remove(i);
-  }
+public class RandomStringChooser {
+    private ArrayList<String> words;
+
+    public RandomStringChooser(String[] wordArray) {
+        words = new ArrayList<String>();
+        for (String w : wordArray) {
+            words.add(w);
+        }
+    }
+
+    public String getNext() {
+        if (words.size() == 0) {
+            return "NONE";
+        } else {
+            int index = (int)(Math.random() * words.size());
+            return words.remove(index);
+        }
+    }
 }
