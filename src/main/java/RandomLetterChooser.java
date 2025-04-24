2796f5a3614d0d4c80680cb5faa1fa9900ee5c5e
mattc489 <machan12@s.sfusd.edu>
2025-04-24 11:25:31 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,28 +1,14 @@
-public class RandomLetterChooser extends RandomStringChooser
-{
-	
-	public RandomLetterChooser(String str)
-	{ 
-	super(getSingleLetters(str));
-	}
-	public static String[]getSingleLetters(String str){
-		String[]letterArray = new String[str.length()];
-		for(int i=0; i<str.length; i++){
-			letterArray[i]=str.charAt(i);
-		}
-		return letterArray;
-	}
-		
-	}
-	
-	
-	public static String[] getSingleLetters(String str)
-	{ 
-		String[] letterArray = new String[str.length()];
-		for(int i=0; i<str.length(); i++)
-		{
-			letterArray[i] = ""+str.charAt(i);
-		}
-		return letterArray;
-	}
+public class RandomLetterChooser extends RandomStringChooser {
+
+    public RandomLetterChooser(String str) {
+        super(getSingleLetters(str));
+    }
+
+    private static String[] getSingleLetters(String str) {
+        String[] letterArray = new String[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            letterArray[i] = "" + str.charAt(i);
+        }
+        return letterArray;
+    }
 }
